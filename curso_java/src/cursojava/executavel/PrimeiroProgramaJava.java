271b69dab2a4949d7f65024cd7b055af2145f1ef
Tiago <Tiago@DESKTOP-GJ5ROQU>
2023-02-28 13:42:37 -0300

    Base Java

---
+++ curso_java/src/cursojava/executavel/PrimeiroProgramaJava.java
@@ -0,0 +1,23 @@
+package cursojava.executavel;
+
+public class PrimeiroProgramaJava {
+
+	public static void main(String[] args) {
+		
+		int maiorIdade = 18;
+		
+		int idosoIdade = 60;
+		
+		int jovemIdade = 16;
+		
+		int criancaIdade = 7;
+		
+		String cpf = "145.785.789-54";
+		
+		System.out.println("Maior idade é = " + maiorIdade);
+		System.out.println("Idoso idade é = " + idosoIdade);
+		System.out.println("Jovem idade é = " + jovemIdade);
+		System.out.println("Criança idade é = " + criancaIdade);
+		System.out.println("CPF = " + cpf);
+	}
+}
